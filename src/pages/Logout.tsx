7c758bbd502aa261e8146b9a1b991e32c39af6c5
jonas-amilton <jonas96@edu.unisinos.br>
2024-11-15 12:43:47 -0300

    fix: corrigido o logout para limpar os dados da sessão

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 const Logout: React.FC = () => {
   useEffect(() => {
     localStorage.removeItem("userEmail");
+    localStorage.setItem("userEmail", "");
 
     window.location.href = "/login";
   }, []);
