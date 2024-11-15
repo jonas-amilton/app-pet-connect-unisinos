4ba020a906f40cd3b6b1061a8349386366a0e015
jonas-amilton <jonas96@edu.unisinos.br>
2024-11-15 14:48:51 -0300

    feat and refactor: criado método de adicionar pet, criado método de retornar foto do pet, removido mensagens de console, adicionado comentários sobre a urlBase remota e local

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api-pet-connect-unisinos.onrender.com",
+  baseURL: "https://api-pet-connect-unisinos.onrender.com", // remota
+  // baseURL: "http://localhost:3000/", // local
 });
 
 export const login = async (email: string, password: string) => {
@@ -10,7 +11,6 @@ export const login = async (email: string, password: string) => {
 
     return response;
   } catch (error: string | any) {
-    console.log("error in login", error);
     throw error;
   }
 };
@@ -30,7 +30,6 @@ export const register = async (
 
     return response;
   } catch (error: string | any) {
-    console.log("error in register", error);
     throw error;
   }
 };
@@ -40,9 +39,37 @@ export const getPets = async () => {
     const response = await api.get("/pets");
     return response.data;
   } catch (error: any) {
-    console.error("Erro ao buscar pets:", error);
     throw error;
   }
+};
+
+export const addPet = async (
+  name: string,
+  age: string,
+  size: string,
+  photo: File
+) => {
+  try {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("age", age);
+    formData.append("size", size);
+    formData.append("photo", photo);
+
+    const response = await api.post("/pets", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  } catch (error: any) {
+    throw error;
+  }
+};
+
+export const getPetPhoto = (photo: string) => {
+  return `${api.defaults.baseURL}${photo}`;
 };
 
 export default api;
