c6dcef04bcfda49cdc03ad30c6acdd005a09742d
jonas-amilton <jonas96@edu.unisinos.br>
2024-11-15 13:04:26 -0300

    refactor: alterado o nome do componente para ficar genérico, e usar nas telas de registro de usuário e registro de pet

--- src/components/InputName.tsx
+++ src/components/InputName.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import { IonInput } from "@ionic/react";
 
-interface InputUsernameProps {
+interface InputNameProps {
   value: string;
   onChange: (e: CustomEvent) => void;
 }
 
-const InputUsername: React.FC<InputUsernameProps> = ({ value, onChange }) => {
+const InputName: React.FC<InputNameProps> = ({ value, onChange }) => {
   const [isTouched, setIsTouched] = useState(false);
   const [isValid, setIsValid] = useState<boolean>();
 
-  const validateUsername = (username: string) => {
-    return username.length >= 3;
+  const validateName = (name: string) => {
+    return name.length >= 3;
   };
 
   const validate = (ev: Event) => {
@@ -21,7 +21,7 @@ const InputUsername: React.FC<InputUsernameProps> = ({ value, onChange }) => {
 
     if (value === "") return;
 
-    validateUsername(value) ? setIsValid(true) : setIsValid(false);
+    validateName(value) ? setIsValid(true) : setIsValid(false);
   };
 
   const markTouched = () => {
@@ -35,10 +35,10 @@ const InputUsername: React.FC<InputUsernameProps> = ({ value, onChange }) => {
       } ${isTouched && "ion-touched"}`}
       type="text"
       fill="solid"
-      label="Nome de usuário"
+      label="Nome"
       labelPlacement="floating"
-      helperText="Digite um nome de usuário válido"
-      errorText="Nome de usuário inválido"
+      helperText="Digite um nome válido"
+      errorText="Nome inválido"
       onIonInput={(event) => onChange(event)}
       onIonBlur={() => markTouched()}
       value={value}
@@ -46,4 +46,4 @@ const InputUsername: React.FC<InputUsernameProps> = ({ value, onChange }) => {
   );
 };
 
-export default InputUsername;
+export default InputName;
